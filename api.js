0a814b9c2c529866d09d192e670cfb4172b229f1
Nagamine <jimmy.kochi@unmsm.edu.pe>
2020-06-10 16:12:35 -0500

    pagina

---
+++ api.js
@@ -0,0 +1,21 @@
+const userAction = async () => {
+  const response = await fetch('http://localhost:4567/hello');
+  const myJson = await response.json(); //extract JSON from the http response
+  showLibros(myJson)
+  // do something with myJson
+}
+function showLibros(jsonObj){
+    console.log(jsonObj);
+    const myPara1 = document.createElement('p');
+   const myPara2 = document.createElement('p');
+   const myPara3 = document.createElement('p');
+  const myPara4 = document.createElement('p');
+   myPara1.textContent = 'Codigo: ' + jsonObj.codigo;
+   myPara2.textContent = 'Nombre: ' + jsonObj.nombre;
+   myPara3.textContent = 'Autor: ' + jsonObj.autor;
+   myPara4.textContent = 'numero Pags: ' + jsonObj.numPag;
+   document.body.appendChild(myPara1);
+   document.body.appendChild(myPara2);
+   document.body.appendChild(myPara3);
+   document.body.appendChild(myPara4);
+}
